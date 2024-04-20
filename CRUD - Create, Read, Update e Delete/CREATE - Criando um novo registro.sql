12d08141a0b9d93ccab3a3a0841fdd917feda431
cesar.tomita <cesar.jun.tomita@gmail.com>
2024-04-20 17:45:15 -0300

    update: crud

--- CRUD - Create, Read, Update e Delete/CREATE - Criando um novo registro.sql
+++ CRUD - Create, Read, Update e Delete/CREATE - Criando um novo registro.sql
@@ -1,4 +1,14 @@
--- Comando para inserir um dado em uma tabela
+
+-- Comando para criar uma tabela chamado PESSOA
+
+CREATE TABLE PESSOA
+(
+    ID INT,
+    NOME VARCHAR(120),
+    EMAIL VARCHAR(120)
+);
+
+-- Comando para inserir um dado na tabela PESSOA
 
 INSERT INTO PESSOA
 (
@@ -8,5 +18,5 @@ INSERT INTO PESSOA
 VALUES
 (
     'José Silva',
-    'josesilva@email.com'
+    'jose.silva@email.com'
 );

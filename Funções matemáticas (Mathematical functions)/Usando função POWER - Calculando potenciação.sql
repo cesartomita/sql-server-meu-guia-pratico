69ca5b40030ab323f992adc0ff342f536b1db414
cesar.tomita <cesar.jun.tomita@gmail.com>
2025-07-19 21:01:23 -0300

    Adicionando as funções: POWER e SQRT

---
+++ Funções matemáticas (Mathematical functions)/Usando função POWER - Calculando potenciação.sql
@@ -0,0 +1,7 @@
+-- A função POWER(base, expoente) retorna a potência da base elevada ao expoente.
+
+SELECT POWER(3, 1) -- 3
+
+SELECT POWER(3, 2) -- 9
+
+SELECT POWER(3, 3) -- 27
---
+++ Funções matemáticas (Mathematical functions)/Usando função SQRT - Retorna a raiz quadrada de um valor.sql
@@ -0,0 +1,7 @@
+-- Retorna a raiz quadrada do valor
+
+SELECT SQRT(4) -- 2
+
+SELECT SQRT(50) -- 7,07106781186548
+
+SELECT SQRT(100) -- 10

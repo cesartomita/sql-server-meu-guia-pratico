4d336c008bd3a2acd0b2f15fd95ad13f9c0a8f8a
cesar.tomita <cesar.jun.tomita@gmail.com>
2025-07-20 19:09:49 -0300

    Adicionando função DATEADD

--- Usando DATEPART, DATEDIFF e DATEADD/Alterando ano, mês e dia copy.sql
+++
@@ -1,7 +0,0 @@
-DECLARE @HORA TIME = GETDATE();
-
-SELECT
-    @HORA,
-    DATEADD(HOUR, 1, @HORA),    -- Adicionando 1 hora
-    DATEADD(MINUTE, 10, @HORA), -- Adicionando 10 minutos
-    DATEADD(SECOND, 30, @HORA); -- Adicionando 30 segundos

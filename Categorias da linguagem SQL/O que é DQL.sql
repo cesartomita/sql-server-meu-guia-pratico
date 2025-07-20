041804db13dd7d0580c80dda978e7175e645538e
cesar.tomita <cesar.jun.tomita@gmail.com>
2025-07-20 19:28:29 -0300

    DCL, DDL, DML, DQL, DTL e TCL

---
+++ Categorias da linguagem SQL/O que é DQL.sql
@@ -0,0 +1,9 @@
+-- DQL – Data Query Language (Linguagem de Consulta de Dados)
+-- É a parte da linguagem SQL responsável por consultar dados armazenados no banco de dados.
+-- SELECT: Recuperar dados de uma tabela.
+
+-- Sintaxe:
+SELECT * FROM [NOME_DA_TABELA];
+
+-- Exemplo:
+SELECT * FROM CLIENTE;
--- Categorias da linguagem SQL/O que é DTL ou TCL.sql
+++ Categorias da linguagem SQL/O que é DTL ou TCL.sql
@@ -1,9 +1,9 @@
 /*
+	DTL	- Linguagem de Transação de Dados (Data Transaction Language)
 	TCL - Comandos de Controle de Transações (Transaction Control Language)
 
 	A TCL (Transaction Control Language) é um subconjunto da SQL responsável por gerenciar as transações no banco de dados.
 	Uma transação é um conjunto de instruções SQL logicamente relacionadas, executadas nos dados armazenados nas tabelas.
-
 */
 
 -- Inicia uma transação. A partir desse ponto, as mudanças feitas no banco de dados estão pendentes até que sejam confirmadas ou revertidas.
